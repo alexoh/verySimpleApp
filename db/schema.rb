7697e44f5f414bec4ca07d15d77d00545c24cd57
Alex <a.ohm@zeppelin-university.net>
2015-07-23 14:38:47 +0200

    added user scaffolding, order relations and devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721124343) do
+ActiveRecord::Schema.define(version: 20150723123223) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20150721124343) do
     t.datetime "updated_at",  null: false
     t.string   "colour"
     t.string   "price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
