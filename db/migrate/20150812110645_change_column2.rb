45cc09ae0cde64db8f72e2096e68ccd87acbc046
Alex <a.ohm@zeppelin-university.net>
2015-08-12 13:31:09 +0200

    changed product price to integer

---
+++ db/migrate/20150812110645_change_column2.rb
@@ -0,0 +1,5 @@
+class ChangeColumn2 < ActiveRecord::Migration
+  def change
+  	change_column :products, :price, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728112103) do
+ActiveRecord::Schema.define(version: 20150812110645) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20150728112103) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
-    t.string   "price"
+    t.integer  "price"
   end
 
   create_table "users", force: :cascade do |t|
