a2622464ae05f53b0a16315645bfd3d0bf6a7ddb
Alex <a.ohm@zeppelin-university.net>
2015-08-12 14:35:47 +0200

    bugfix finished

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,7 @@
 if Rails.env.production?
 	Rails.configuration.stripe = {
-		# :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-		# :secret_key => ENV['STRIPE_SECRET_KEY']
-		:publishable_key => 'pk_test_aVf5YZQzKbJ9xStdQBDNNQ0v',
-		:secret_key => 'sk_test_67wfHCtYFUztLfiyiQF4h4PR'
+		:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+		:secret_key => ENV['STRIPE_SECRET_KEY']
 	}
 else
 	Rails.configuration.stripe = {
