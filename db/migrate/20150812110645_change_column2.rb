bf01f265a240848066d0053e24d872b743aa6320
Alex <a.ohm@zeppelin-university.net>
2015-08-20 12:05:57 +0200

    changed migration file

--- db/migrate/20150812110645_change_column2.rb
+++ db/migrate/20150812110645_change_column2.rb
@@ -1,5 +1,5 @@
 class ChangeColumn2 < ActiveRecord::Migration
   def change
-  	change_column :products, :price, :integer
+  	change_column :products, :price, :integer TYPE integer USING (:price::integer)
   end
 end
